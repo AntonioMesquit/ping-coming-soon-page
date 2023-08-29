25584ea0d59044b7f4f13c26537747b21acad1a5
Antonio Jose Mesquita Silveira <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-29 07:56:03 -0300

    up

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0
 
 const email = document.querySelector("#email")
 const send = document.querySelector(".but")
+const inputs = document.querySelector(".inputs");
 
 function validacao(input){
     const formInput = input.parentElement;
@@ -9,14 +10,17 @@ function validacao(input){
     if(input === "" || input === undefined){
         formInput.classList.add("erro");
         erros.classList.add("active")
+        inputs.classList.add("erro")
     }
     else if(!input.value.match(regexEmail)){
         formInput.classList.add("erro");
         erros.classList.add("active")
+        inputs.classList.add("erro")
     }
     else{
         formInput.classList.remove("erro")
         erros.classList.remove("active")
+        inputs.classList.remove("erro")
     }
 
 }
